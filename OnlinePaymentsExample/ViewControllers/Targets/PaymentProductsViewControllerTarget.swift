04fee8975c9f1d60ee8790679217b582171acc6a
Worldline Direct support team <82139942+worldline-direct-support-team@users.noreply.github.com>
2026-06-09 09:50:41 +0200

    Release 2.4.1

--- OnlinePaymentsExample/ViewControllers/Targets/PaymentProductsViewControllerTarget.swift
+++ OnlinePaymentsExample/ViewControllers/Targets/PaymentProductsViewControllerTarget.swift
@@ -1,11 +1,14 @@
-//
-//  PaymentProductsViewControllerTarget.swift
-//  OnlinePaymentsExample
-//
-//  Created for Online Payments on 15/12/2016.
-//  Copyright © 2016 Global Collect Services. All rights reserved.
-//
-
+/*
+ * Do not remove or alter the notices in this preamble.
+ *
+ * This software is owned by Worldline and may not be be altered, copied, reproduced, republished, uploaded, posted, transmitted or distributed in any way, without the prior written consent of Worldline.
+ *
+ * Copyright © 2026 Worldline and/or its affiliates.
+ *
+ * All rights reserved. License grant and user rights and obligations according to the applicable license agreement.
+ *
+ * Please contact Worldline for questions regarding license and user rights.
+ */
 import Foundation
 import PassKit
 import OnlinePaymentsKit
